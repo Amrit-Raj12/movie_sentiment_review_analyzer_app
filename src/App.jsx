911a67ee919601fff7d904c00062d6d167a9215e
Amrit-Raj12 <amrit.raj1224@gmail.com>
2025-09-09 12:50:41 +0530

    Updated code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MovieSentimentAnalyzer from './components/MovieSentimentAnalyzer'
+
+function App() {
+
+  return (
+    <>
+      <MovieSentimentAnalyzer />
+          </>
+  )
+}
+
+export default App
